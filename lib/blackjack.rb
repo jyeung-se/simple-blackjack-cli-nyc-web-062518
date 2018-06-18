e3fd5a24862f1fbf0a3eeae49e054f4e04b332c3
Jack Yeung <jyeung.se@gmail.com>
2018-06-18 01:09:09 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,8 +34,11 @@ end
 def hit?(number)
   prompt_user
   get_user_input
-  if get_user_input == "s"
-    
+  if get_user_input == "h"
+    deal_card
+    card_total += deal_card
+  else
+  end
 end
 
 def invalid_command
