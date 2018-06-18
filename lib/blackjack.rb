1634e04978e2ae3b046d456d92ddfc2628a34581
Jack Yeung <jyeung.se@gmail.com>
2018-06-18 02:21:23 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -24,9 +24,9 @@ def end_game(card_total)
 end
 
 def initial_round
-  deal_card
-  deal_card
-  card_total = deal_card + deal_card
+  card_one = deal_card
+  card_two = deal_card
+  card_total = card_one + card_two
   display_card_total(card_total)
   card_total
 end
